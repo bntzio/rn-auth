eb8cf515d7f2f41338e7f8a90981ebef38bb23d9
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-11-28 19:37:14 -0600

    render logout button if user is logged in

--- src/app.js
+++ src/app.js
@@ -2,22 +2,63 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import firebaseKeys from './firebase/keys';
-import { Header } from './components/common/';
+import { Header, Button, Spinner } from './components/common/';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp(firebaseKeys);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={styles.logoutButtonStyle}>
+            <Button>
+              Log Out
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.spinnerStyle}>
+            <Spinner size="large" />
+          </View>
+        );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  spinnerStyle: {
+    height: 420
+  },
+  logoutButtonStyle: {
+    marginTop: 40,
+    height: 40
+  }
+};
 
 export default App;
